c67707924171aee8aac6758ab57cc3441919607c
Oshi99 <oshiryaeva@protonmail.com>
2018-07-23 20:54:14 -0500

    second commit

--- how_many?.rb
+++ how_many?.rb
@@ -1,6 +1,6 @@
 vehicles = ['car', 'car', 'truck', 'car', 'SUV', 'truck', 'motorcycle', 'motorcycle', 'car', 'truck']
 
 def count_occurances(arr)
-	arr.uniq.each {|word| puts "#{word} => #{arr.count(word)}"}
+  arr.uniq.each {|word| puts "#{word} => #{arr.count(word)}"}
 end
 count_occurances(vehicles)

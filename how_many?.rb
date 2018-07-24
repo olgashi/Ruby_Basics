e86b074d3a9c0da406fe5a2e28a6605e694c6814
Oshi99 <oshiryaeva@protonmail.com>
2018-07-23 20:51:25 -0500

    updating spaces

--- how_many?.rb
+++ how_many?.rb
@@ -1,10 +1,3 @@
-# make array of unique elements from given array
-
-# take each unique element and count how many of 
-# such elements are in a given array
-
-# print out each unique element and number of occurances
-
 vehicles = ['car', 'car', 'truck', 'car', 'SUV', 'truck', 'motorcycle', 'motorcycle', 'car', 'truck']
 
 def count_occurances(arr)
